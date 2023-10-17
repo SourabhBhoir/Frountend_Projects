5242c1e7742b1b0382a5138ef2408126b42af637
SourabhBhoir <122167585+SourabhBhoir@users.noreply.github.com>
2023-10-17 21:24:40 +0530

    Add files via upload

---
+++ Frountend Projects/COUNTER_APP/COUNTER_APP.js
@@ -0,0 +1,15 @@
+let count = 0;
+
+document.getElementById("decrement").onclick = function(){
+    count -= 1;
+    document.getElementById("counter").innerHTML = count;
+}
+document.getElementById("reset").onclick = function(){
+    count = 0;
+    document.getElementById("counter").innerHTML = count;
+}
+document.getElementById("increment").onclick = function(){
+    count += 1;
+    document.getElementById("counter").innerHTML = count;
+}
+
